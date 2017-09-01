0bc56089a01f0cd690d6b53fa8113c8374fd544c
Peter Shen <peter.shen@aimsio.com>
2017-08-31 23:07:22 -0400

    assignment 1 completed

--- Lecture1_RDBS/assigment1.sql
+++ Lecture1_RDBS/assigment1.sql
@@ -1,4 +1,4 @@
-Question 1.
+Question 1. -----
 a)
 select count(*) from `patients` where `patients`.`sex` = 'F';
 
@@ -37,7 +37,7 @@ where `A`.`count` = 2;
 
 395
 
-Question 2:
+Question 2: ---
 a)
 select count(*) from `patients`
 join `pmh` on `patients`.id = `pmh`.id
@@ -77,15 +77,108 @@ select count(`patients`.id) from `patients`
     where `pmh`. hyperlipid = 1
 3975
 
-patients with hyperlipid who are taking drugs
-select * from `drugs` where `drugs`.id in (
+## patients with hyperlipid who are taking any drugs
+
+select count(*) from (select id from `drugs` where `drugs`.id in (
     select `patients`.id from `patients`
     join `pmh` on `patients`.id = `pmh`.id
-    where `pmh`.hyperlipid = 1)
+    where `pmh`.hyperlipid = 1) group by id) as A
 
 3463
 3975 - 3463 = 512 who are not taking drugs
 
 512 / 3975 = 12.9%
 
-select drug from `drugclasses` where `drugclasses`.level2 = 'Metabolic agents; antihyperlipidemia agents';
+Question 2 - Using just males ----
+a)
+select count(*) from `patients`
+join `pmh` on `patients`.id = `pmh`.id
+where `pmh`.diabetes = 1 and `patients`.sex = 'M';
+
+1336
+
+select count(*) from `patients`
+join `pmh` on `patients`.id = `pmh`.id
+join `tests` on `tests`.id = `patients`.id
+where `pmh`.diabetes = 1 and `tests`.test = 'hba1c' and `patients`.sex = 'M';
+
+207
+
+207/1336 = 15.5%
+
+b)
+select count(*) from `patients`
+join `pmh` on `patients`.id = `pmh`.id
+where `pmh`.htn = 1 and `patients`.sex = 'M';
+
+3072
+
+select count(*) from `patients` where id in (
+    select `patients`.id from `patients`
+    join `pmh` on `patients`.id = `pmh`.id
+    where `pmh`.htn = 1 and `patients`.sex = 'M')
+and sbp is null or sbp = '';
+
+887
+
+887/3072 = 28.9%
+
+c)
+select count(`patients`.id) from `patients`
+    join `pmh` on `patients`.id = `pmh`.id
+    where `pmh`. hyperlipid = 1 and `patients`.sex = 'M'
+1887
+
+## patients with hyperlipid who are taking any drugs
+
+select count(*) from (select id from `drugs` where `drugs`.id in (
+    select `patients`.id from `patients`
+    join `pmh` on `patients`.id = `pmh`.id
+    where `pmh`.hyperlipid = 1 and `patients`.sex = 'M') group by id) as A
+
+1623
+1887 - 1623 = 264 who are not taking drugs
+
+264 / 1887 = 14.0%
+
+Overall, recommended interventions are less followed now compared to 2003.
+
+Question 3 -----
+select count(sbp) from patients where sbp like '%0';
+5908
+select count(sbp) from patients where sbp is not null or sbp != '';
+15630
+
+select count(dbp) from patients where dbp like '%0';
+6208
+select count(dbp) from patients where dbp is not null or dbp != '';
+15618
+
+Do another pass on other numerics
+0 5908
+1 416
+2 2255
+3 392
+4 1601
+5 549
+6 1400
+7 445
+8 2201
+9 463
+
+There does seem to be a preference for 0
+
+4.
+Our surroundings such the food we eat, the cultures we follow, and our lifestyles can attribute to certain disease progressions.
+Which region has the highest number of patients diagnosed with hyperlipid?
+What age group do most of these patients belong to?
+
+select `patients`.region as 'Region', count(*) as `Number of Patients` from `patients` join `pmh` on `patients`.id = `pmh`.id where `pmh`.hyperlipid = 1 group by `patients`.region;
+Region	Number of Patients
+Midwest	880
+Northeast	935
+South	1311
+West	849
+
+select `patients`.age, count(*) as `Number of Patients` from `patients` join `pmh` on `patients`.id = `pmh`.id where `pmh`.hyperlipid = 1 and `patients`.region = 'South' group by age order by `Number of Patients` desc
+Most of these patients are from late 50s to early 70s.
